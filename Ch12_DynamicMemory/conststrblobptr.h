5a7cca24a1783ee36f20e1302332f6c276dc527e
rockpaperbanan <rockpaperbanan@gmail.com>
2017-07-10 22:38:56 +0200

    modify conststrblobptr.h

--- Ch12_DynamicMemory/conststrblobptr.h
+++ Ch12_DynamicMemory/conststrblobptr.h
@@ -22,38 +22,22 @@ public:
     ConstStrBlobPtr(const StrBlob& a, size_t sz=0)
         : wptr(a.data), curr(sz) { }
 
-<<<<<<< HEAD
-    const std::string& deref() const;   // dereference as const
-    ConstStrBlobPtr& incr(); // prefix version
-    bool operator==(const ConstStrBlobPtr& rhs)
-        { return curr == rhs.curr; }
-    bool operator!=(const ConstStrBlobPtr& rhs)
-        { return curr != rhs.curr; }
-=======
     const std::string& deref() const;
     ConstStrBlobPtr& incr(); // prefix version
     bool operator==(const ConstStrBlobPtr& rhs) const
         { return curr == rhs.curr; }
     bool operator!=(const ConstStrBlobPtr& rhs) const
         { return !(*this == rhs); }
->>>>>>> 542e774ef888abf418964a6997031e98a15bf5f6
 private:
 // check returns a shared_ptr to the vector if check succeeds
     std::shared_ptr<std::vector<std::string>>
         check(std::size_t, const std::string&) const;
 
-<<<<<<< HEAD
-    std::weak_ptr<std::vector<std::string>> wptr;     // constant reference;
-    std::size_t curr;       // current position within the array
-};
-
-=======
     std::weak_ptr<std::vector<std::string>> wptr;
     std::size_t curr;       // current position within the array
 };
 
 
->>>>>>> 542e774ef888abf418964a6997031e98a15bf5f6
 inline std::shared_ptr<std::vector<std::string>>
 ConstStrBlobPtr::check(std::size_t i, const std::string& msg) const
 {
@@ -77,11 +61,7 @@ inline const std::string& ConstStrBlobPtr::deref() const
 inline ConstStrBlobPtr& ConstStrBlobPtr::incr()
 {
     // if curr already points past the end of the container can't increment it
-<<<<<<< HEAD
-    check(curr, "increment past end of StrBlobPtr");
-=======
     check(curr, "increment past end of ConstStrBlobPtr");
->>>>>>> 542e774ef888abf418964a6997031e98a15bf5f6
     ++curr;
     return *this;
 }
@@ -91,8 +71,4 @@ inline ConstStrBlobPtr StrBlob::cbegin() const
 inline ConstStrBlobPtr StrBlob::cend() const
     { return ConstStrBlobPtr(*this, data->size()); }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 542e774ef888abf418964a6997031e98a15bf5f6
 #endif /*CONSTSTRBLOBPTR_H_*/
